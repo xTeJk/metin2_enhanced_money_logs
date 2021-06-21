336914707891cc4382e71c77b4c337efd1523aeb
Kamil <32514182+xTeJk@users.noreply.github.com>
2021-06-21 18:20:13 +0200

    Update shop_manager.cpp

--- source/game/shop_manager.cpp
+++ source/game/shop_manager.cpp
@@ -2,7 +2,7 @@
 	DBManager::instance().SendMoneyLog(MONEY_LOG_SHOP, item->GetVnum(), dwPrice);
 
 //REPLACE WITH:
-#ifdef
+#ifdef TAKE_ENHANCED_MONEY_LOGS
 	char NPCShopSellLog[512];
 	snprintf(NPCShopSellLog, sizeof(NPCShopSellLog), "item_count: %u", item->GetCount());
 	LogManager::instance().NEWMoneyLog(ch->GetPlayerID(), ch->GetName(), "NPC_SHOP_SELL", NPCShopSellLog, item->GetVnum(), 0, "", dwPrice, ch->GetGold(), ch->GetGold()+dwPrice);
